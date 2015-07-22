0385d291be8b86de6873302c9a2c848468ac37c2
Hiroyuki Ito <hageyahhoo@gmail.com>
2015-07-22 21:49:58 +0900

    Investigated data_bags. Always need id...

--- site-cookbooks/config/recipes/default.rb
+++ site-cookbooks/config/recipes/default.rb
@@ -5,7 +5,19 @@
 # Copyright 2015, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-#
+
+# Test for data_bag
+# * Need "id" in each json file.
+ids = data_bag('users')
+
+ids.each do |id|
+  user = data_bag_item('users', id)
+  log user['id']
+  log user['color']
+end
+
+
+# Template for .bash_profile
 template "/home/vagrant/.bash_profile" do
   owner node['user']
   group node["group"]
